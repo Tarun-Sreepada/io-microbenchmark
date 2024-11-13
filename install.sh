d9f8e5056152ec4662b1cee6250d8bd824f43959
Tarun Sreepada <tarunsreepada@tutanota.com>
2024-11-13 18:37:00 +0900

    test

---
+++ install.sh
@@ -0,0 +1,15 @@
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-keyring_1.1-1_all.deb
+sudo dpkg -i cuda-keyring_1.1-1_all.deb
+sudo apt-get update
+sudo apt-get -y install cuda-toolkit-12-6
+sudo apt-get install -y nvidia-open
+
+sudo apt install git
+git clone https://github.com/Tarun-Sreepada/io-microbenchmark.git
+
+sudo apt install curl
+wget "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
+bash Miniforge3-$(uname)-$(uname -m).sh
+
+sudo apt install cmake
+sudo apt install liburing-dev
